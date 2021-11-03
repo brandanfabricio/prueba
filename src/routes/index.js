7e7d3824ae727db06ba80313431361030c8f5b67
Fabricio Brandan <brandanfabricio7@gmai.com>
2021-11-03 18:54:33 -0300

    pagina index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,19 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
-
 module.exports = function({HomeRoutes,IdeasRoutes}){
         const router = express.Router();
         const routerMidel = express.Router();
+
 
         routerMidel
         .use(express.json())
         .use(cors())
         .use(helmet())
+
         .use(compression());
 
         routerMidel.use('/home',HomeRoutes);
         routerMidel.use('/ideas',IdeasRoutes);
+        
         router.use('/v1/api',routerMidel);
 
 
--- src/startup/servidor.js
+++ src/startup/servidor.js
@@ -8,7 +8,7 @@ let _config = null;
 class Server {
     constructor({Config ,Rutas}){
         _config = Config;
-        _express = express().use(Rutas);
+        _express = express().use(Rutas).use(express.static('public'))
     }
     start(){
         return new Promise(resolve => {
