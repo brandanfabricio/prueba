23cd972efc64a8ceb3d264c0860879ed4ace05c3
Fabricio Brandan <brandanfabricio7@gmai.com>
2021-11-03 20:24:13 -0300

    hola

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
+//const helmet = require('helmet');
+//const compression = require('compression');
 module.exports = function({HomeRoutes,IdeasRoutes}){
         const router = express.Router();
         const routerMidel = express.Router();
@@ -10,9 +10,9 @@ module.exports = function({HomeRoutes,IdeasRoutes}){
         routerMidel
         .use(express.json())
         .use(cors())
-        .use(helmet())
+       // .use(helmet())
 
-        .use(compression());
+        //.use(compression());
 
         routerMidel.use('/home',HomeRoutes);
         routerMidel.use('/ideas',IdeasRoutes);
